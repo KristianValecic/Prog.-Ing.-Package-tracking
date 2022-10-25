12e582300fa65703bd3f9e699179805c2510ac53
KristianValecic <79859420+KristianValecic@users.noreply.github.com>
2022-10-25 17:11:58 +0200

    Initial commit

---
+++ src/main/java/hr/algebra/programskoinzenjerstvo/packagetracking/PackageTrackingApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.programskoinzenjerstvo.packagetracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageTrackingApplication.class, args);
+	}
+
+}
